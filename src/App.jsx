007138b095f4493e3825ffcbf124e90502b50437
IraMira333 <irinat333@gmail.com>
2024-01-05 22:49:50 +0200

    Hello World in App

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,5 @@ import CatalogPage from 'pages/CatalogPage';
 
 export default function App() {
   console.log('Hello World');
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="catalog" element={<CatalogPage />} />
-        <Route path="favorites" element={<FavoritePage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Route>
-    </Routes>
-  );
+  return <h1>Hello World</h1>;
 }
