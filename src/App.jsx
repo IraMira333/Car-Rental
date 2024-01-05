f4da10b735ca9836a6ce0bd30c26b6095a191a2e
IraMira333 <irinat333@gmail.com>
2024-01-05 20:48:13 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from 'pages/HomePage/HomePage';
-import CatalogPage from 'pages/CatalogPages';
 import FavoritePage from 'pages/FavoritePage';
+import CatalogPage from 'pages/CatalogPage';
 
 export default function App() {
   console.log('Hello World');
